cbb4079ef8f6942632428924e9088bc1d7af1ace
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2025-09-11 16:37:08 +0530

    Update timer.jsx

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -4,7 +4,7 @@ const countdownTargets = {
   submission: new Date('2025-09-26T23:59:59'),
   registration: new Date('2025-09-30T23:59:59'),
   conference: new Date('2025-11-07T09:00:00'),
-  acceptance: new Date('2025-09-20T09:00:00'),
+  acceptance: new Date('2025-09-26T09:00:00'),
 };
 
 const getTimeLeft = (targetDate) => {
